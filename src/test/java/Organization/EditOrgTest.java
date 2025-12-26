819933e17f719906995af423efaec28fb9f7cabd
HP <HP@LAPTOP-JMVUU0MR>
2025-12-26 16:23:42 +0530

    edit org

---
+++ src/test/java/Organization/EditOrgTest.java
@@ -0,0 +1,12 @@
+package Organization;
+
+import org.testng.annotations.Test;
+
+public class EditOrgTest 
+{
+   @Test
+   public void editOrg()
+   {
+	   System.out.println("-----edit-----");
+   }
+}
