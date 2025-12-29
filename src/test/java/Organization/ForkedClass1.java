cbdb5d8139f5fa0305218b900dbe837d33a76001
NAGARAJ <NAGARAJ@DESKTOP-IODHK5L>
2025-12-29 14:39:14 +0530

    forked

---
+++ src/test/java/Organization/ForkedClass1.java
@@ -0,0 +1,11 @@
+package Organization;
+
+public class ForkedClass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("--Forked1--");
+System.out.println("forked2");
+	}
+
+}
