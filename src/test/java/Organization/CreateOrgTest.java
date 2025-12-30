50916d7d87eb4c3dc299bc3c35e25a2bab55be24
HP <HP@LAPTOP-JMVUU0MR>
2025-12-30 15:58:04 +0530

    i edited in testscripts

--- src/test/java/Organization/CreateOrgTest.java
+++ src/test/java/Organization/CreateOrgTest.java
@@ -5,6 +5,7 @@ public class CreateOrgTest
     public static void main(String[] args) 
     {
     	System.out.println("Hi");
+    	System.out.println("bye");
 		//i am making changes in eclipse createorg
 	}
 }
