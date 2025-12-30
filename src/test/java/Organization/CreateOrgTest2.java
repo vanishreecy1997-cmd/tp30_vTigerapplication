1ba48452ea958f5aed999c18a72f75444c1cb35f
HP <HP@LAPTOP-JMVUU0MR>
2025-12-31 00:22:03 +0530

    i added one testscript

---
+++ src/test/java/Organization/CreateOrgTest2.java
@@ -0,0 +1,11 @@
+package Organization;
+
+public class CreateOrgTest2 
+{
+    public static void main(String[] args) 
+    {
+    	System.out.println("Hi");
+    	System.out.println("bye");
+		//i am making changes in eclipse createorg
+	}
+}
